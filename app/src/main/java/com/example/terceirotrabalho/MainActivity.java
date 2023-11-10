b4b863b63b12d6767964fc8df0c90288af369198
Dener Fernandes <dener.fernandes.oliveira@gmail.com>
2023-11-09 22:32:22 -0400

    feat: create-login-fragment

--- app/src/main/java/com/example/terceirotrabalho/MainActivity.java
+++ app/src/main/java/com/example/terceirotrabalho/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.terceirotrabalho;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.terceirotrabalho.fragments.LoginFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    LoginFragment loginFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginFragment = new LoginFragment();
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frameLayout, loginFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
+
+
 }
